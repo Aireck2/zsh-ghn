96e3f062ec87db33a2ef412e992b12f48a193189
@erick <erick.hx22@gmail.com>
2021-06-16 16:06:41 -0500

    chore: update zsh for ghq get (#3)

--- zsh-ghn.zsh
+++ zsh-ghn.zsh
@@ -8,7 +8,7 @@ GHQ_CACHE_DIR="${HOME}/.cache/ghq"
 GHQ_CACHE_NAME="ghq.txt"
 GHQ_CACHE_PROJECT="${GHQ_CACHE_DIR}/${GHQ_CACHE_NAME}"
 
-# GHQ_REGEX_IS_REPOSITORY="^(git:|git@|ssh://|http://|https://)"
+GHQ_REGEX_IS_REPOSITORY="^(git:|git@|ssh://|http://|https://)"
 # GITHUB_USER="$(git config github.user)"
 
 # ghq_package_name='ghq'
@@ -48,6 +48,40 @@ function ghq::cache::list() {
 }
 
 # ghq get simplify
+function ghq::new::template() {
+    local template repository_path
+    repository_path="$(ghq root)/github.com/${GITHUB_USER}/"
+    template="$(ghq::cookiecutter::find)"
+    if [ -z "${template}" ]; then
+        message_warning "Please Select one Project"
+        return
+    fi
+    cd "${repository_path}" || cd - &&
+        eval "cookiecutter ${template}" &&
+        ghq::cache::clear
+}
+
+function ghq::cookiecutter::find() {
+    local command_value
+    command_value=$(
+        ghq::cookiecutter::list |
+            fzf |
+            awk 'BEGIN{FS="\t"; OFS=""} {print $4}' |
+            ghead -c -1
+    )
+
+    if [ -n "${command_value}" ]; then
+        echo -e "${command_value}"
+    fi
+}
+
+function ghq::cookiecutter::list() {
+    # shellcheck disable=SC2002
+    cat "${GHQ_FILE_COOKIECUTTER}" |
+        jq -r '.projects[] | [.name, .type, .description, .repository] | @tsv' |
+        sed 's/"//g'
+}
+
 function ghq::new() {
     local repository repository_path
     repository="${1}"
@@ -57,12 +91,32 @@ function ghq::new() {
     cd "${repository_path}" || cd - && git flow init -d
 }
 
+function ghq::factory() {
+    local repository repository_path is_repository
+    repository="${1}"
+    is_repository=$(echo "${repository}" | grep -cE "${GHQ_REGEX_IS_REPOSITORY}")
+
+    if [ -z "${repository}" ]; then
+        ghq::new::template
+        return
+    fi
+
+    if [ "${is_repository}" -eq 1 ]; then
+        ghq get "${repository}"
+        ghq::cache::clear
+        return
+    fi
+
+    ghq::new "${repository}"
+
+}
+
 function ghq::cache::clear() {
     [ -e "${GHQ_CACHE_PROJECT}" ] && rm -rf "${GHQ_CACHE_PROJECT}"
     ghq::cache::create::factory
 }
 
-alias ghn=ghq::new
+alias ghn=ghq::factory
 
 zle -N ghq::find::project
 bindkey '^P' ghq::find::project
